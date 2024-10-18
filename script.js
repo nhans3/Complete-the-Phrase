305c89ddf67cf25cd174594581b5de9da7c67f10
nhans3 <n.hans3@share.epsb.ca>
2024-10-18 15:04:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Complete the Phrase
+
+document.getElementById("cats-btn").addEventListener("click", cats);
+
+function cats() {
+  alert("dogs.");
+}
+
+document.getElementById("arm-btn").addEventListener("click", arm);
+
+function arm() {
+  alert("and a leg.");
+}
+
+document.getElementById("calm-btn").addEventListener("click", calm);
+
+function calm() {
+  alert("the storm.");
+}
+
+document.getElementById("thick-btn").addEventListener("click", thick);
+
+function thick() {
+  alert("thin.");
+}
+
+document.getElementById("devil-btn").addEventListener("click", devil);
+
+function devil() {
+  alert("devil.");
+}
